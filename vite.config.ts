66d6661292e539def3957ee7011aed06e1fee401
Hahaa13 <ertdgh123@gmail.com>
2026-01-18 00:21:15 +0700

    up

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  server: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp'
-    }
-  },
-  plugins: [
-    tailwindcss(),
-  ],
+    base: "/PyRunner/",
+    server: {
+        headers: {
+        'Cross-Origin-Opener-Policy': 'same-origin',
+        'Cross-Origin-Embedder-Policy': 'require-corp'
+        }
+    },
+    plugins: [
+        tailwindcss(),
+    ],
 })
