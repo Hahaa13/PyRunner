e02beab9ed78980bad9390a4dcd3041e37fc4325
Hahaa13 <ertdgh123@gmail.com>
2026-01-18 00:45:00 +0700

    Remove base path configuration from Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    base: "/PyRunner/",
     server: {
         headers: {
         'Cross-Origin-Opener-Policy': 'same-origin',
