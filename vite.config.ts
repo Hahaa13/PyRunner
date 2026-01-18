83bfe84b645f606e43ae529e80e55583a7db712f
Hahaa13 <ertdgh123@gmail.com>
2026-01-18 07:55:36 +0700

    feat: add PWA support with vite-plugin-pwa and remove old manifest file
    - Added vite-plugin-pwa to devDependencies for PWA capabilities.
    - Integrated PWA configuration in vite.config.ts, including a new manifest.
    - Removed the old manifest.json file as it is now included in the Vite PWA setup.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa';
+
 
 export default defineConfig({
     worker: {
@@ -13,5 +15,57 @@ export default defineConfig({
     },
     plugins: [
         tailwindcss(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                "name": "PyRunner",
+                "short_name": "PyRunner",
+                "theme_color": "#1E1E1E",
+                "background_color": "#1E1E1E",
+                "display": "standalone",
+                "start_url": "/",
+                "icons": [
+                    {
+                        "src": "/python.png",
+                        "sizes": "512x512",
+                        "type": "image/png"
+                    }
+                ]
+            },
+            workbox: {
+                maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10 MB
+                runtimeCaching: [
+                    {
+                        urlPattern: ({ url }) => /pyodide/.test(url.hostname),
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'pyodide-cache',
+                            expiration: {
+                                maxAgeSeconds: 60 * 60 * 24 * 30, // cache for 30 days
+                            },
+                        },
+                    },
+                    {
+                        urlPattern: ({ url }) => {
+                            return /pyodide|monaco/.test(url.hostname);
+                        },
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'assets-cache',
+                            expiration: {
+                                maxAgeSeconds: 60 * 60 * 24 * 30, // cache for 30 days
+                            },
+                        },
+                    },
+                    {
+                        urlPattern: ({ url }) => url.pathname.startsWith('/'),
+                        handler: 'NetworkFirst', // cache first for your application files
+                        options: {
+                            cacheName: 'app-shell-cache',
+                        },
+                    },
+                ]
+            }
+        })
     ],
 })
